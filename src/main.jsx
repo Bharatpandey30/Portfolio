52d47fdf8790ea3ee4aa3f9acf75c030f81ae220
Bharatkumar Pandey <Bharatpandey361@gmail.com>
2024-05-29 17:03:51 -0400

    Update to React 18 createRoot and configure router basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,37 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import App from './App';
+
+
+// const rootElement = document.getElementById('root');
+
+// const router = (
+//   <Routes>
+//     <Route path="/" element={<App/>} />
+//   </Routes>
+// );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>{router}</Router>
+//   </React.StrictMode>,
+//   rootElement
+// );
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-const rootElement = document.getElementById('root');
-
-const router = (
-  <Routes>
-    <Route path="/" element={<App />} />
-  </Routes>
-);
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <Router>{router}</Router>
-  </React.StrictMode>,
-  rootElement
+    <BrowserRouter basename="/Portfolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
